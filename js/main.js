b8bdded84cd4b5860c94a78725347b38a1a0e89b
kfaultner <kfaultner@gmail.com>
2016-04-13 21:47:45 -0700

    updated files

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+	//$('#carousel').hide();
+
+	var t = setInterval(function(){
+		$("#carousel ul").animate({marginLeft:-480},1000,function(){
+		//$(this).find("li:last").after($(this).find("li:first"));
+		//$(this).css({marginLeft:0});
+		})
+	}, 4000);
+
+
+
+});
---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	var t = setInterval(function(){
+		$("#carousel ul").animate({marginLeft:-480},1000,function(){
+		$(this).find("li:last").after($(this).find("li:first"));
+		$(this).css({marginLeft:0});
+		})
+	}, 5000);
+
+});
